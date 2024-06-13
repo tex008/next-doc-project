e4808579c21f426e5c7489ad7e030da81e93b564
tex008 <texera009@gmail.com>
2024-06-13 11:52:17 -0300

    add files into repository

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+});
